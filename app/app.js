8aedb677bbe4cf0520332b5dd523465801f0c644
Jill Agosto <blasagosto@live.com>
2022-03-29 22:33:42 -0500

    Culmination of work
    I still don't know how to properly make branches seperated from each
    other, and I failed a lot in the beggining so I ended up deleting
    everything and starting from scratch even though we were told not to
    but I didn't ask for help until the day before this was due.
    
    Lesson learned.
    
    the project is not complete but this is what I have so far.
    
    night

--- app/app.js
+++ app/app.js
@@ -1,9 +1,54 @@
+'use strict'
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+const authEvents = require('./books/event.js')
+
+// const movieEvents = require('./movies/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  // attach event listeners
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-button').on('click', authEvents.onSignOut)
+  $('.col-4').on('click', function () {
+    const stampyBoi = nameless()
+    $(this).data('value', stampyBoi)
+    $(this).text(stampyBoi)
+  })
+
+  $('.col-4').on('click', function () { const value = $(this).data('value'); console.log(value) })
+
+  const currentPlayer = ['x', 'o']
+  function nameless () {
+    currentPlayer.push(currentPlayer.shift())
+    return currentPlayer[0]
+  }
+
+  function isWeener () {
+    const weeners = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    // for (const combination of winningCombinations) {
+    //   const [a, b, c] = combination
+
+    //   if (this.board[a] && (this.board[a] === this.board[b] && this.board[a] === this.board[c])) {
+    //     return combination
+    //   }
+    // }
+  }
+  // const element = document.getElementById('zero')
+  // document.getElementById('#zero')
+  // document.getElementById('currentPlayer').innerHTML = currentPlayer[]
 })
