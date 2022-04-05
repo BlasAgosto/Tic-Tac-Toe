6e7a85972bb168c078d3d3bf0aad5425a3402eaa
Jill Agosto <blasagosto@live.com>
2022-04-04 21:24:08 -0500

    day 2
    added winning logic
        single click cells
        winner name
        removal of board at end

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 'use strict'
 
+const bootstrap = require('bootstrap')
+const addNestedValue = require('../lib/add-nested-value.js')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./books/event.js')
-
+const pain = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+// const convoluted = ['0a', '1b', '2c', '3d', '4e', '5f', '6g', '7h', '8i']
 // const movieEvents = require('./movies/events.js')
 
 // use require without a reference to ensure a file is bundled
@@ -15,40 +18,101 @@ $(() => {
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
   $('#sign-out-button').on('click', authEvents.onSignOut)
+
   $('.col-4').on('click', function () {
     const stampyBoi = nameless()
+
     $(this).data('value', stampyBoi)
+    const clickyBoi = $(this).attr('id')
+    // console.log(stampyBoi + "hue")
+
+    pain[clickyBoi] = stampyBoi
+    console.log(pain)
+
     $(this).text(stampyBoi)
+    // if (pain[0] === 'x') {
+    //   console.log('without love')
+    // }
+    wheener()
+    stuffed(clickyBoi)
+    // leHide()
   })
 
   $('.col-4').on('click', function () { const value = $(this).data('value'); console.log(value) })
 
-  const currentPlayer = ['x', 'o']
+  const currentPlayer = ['o', 'x']
   function nameless () {
     currentPlayer.push(currentPlayer.shift())
     return currentPlayer[0]
   }
-
-  function isWeener () {
-    const weeners = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
-    // for (const combination of winningCombinations) {
-    //   const [a, b, c] = combination
-
-    //   if (this.board[a] && (this.board[a] === this.board[b] && this.board[a] === this.board[c])) {
-    //     return combination
-    //   }
-    // }
-  }
-  // const element = document.getElementById('zero')
-  // document.getElementById('#zero')
-  // document.getElementById('currentPlayer').innerHTML = currentPlayer[]
 })
+function stuffed (id) {
+  // if (pain[7] === 'x' || pain[7] === 'o') {
+  // console.log(id)
+  $(`#${id}`).off()
+  // $('#' + id).off()
+  // $('#7').off()
+  // }
+}
+function wheener () {
+  if (pain[0] === pain[1] && pain[0] === pain[2]) { dumb(dumber(0)) }
+  if (pain[3] === pain[4] && pain[3] === pain[5]) { dumb(dumber(3)) }
+  if (pain[6] === pain[7] && pain[6] === pain[8]) { dumb(dumber(6)) }
+  if (pain[0] === pain[3] && pain[0] === pain[6]) { dumb(dumber(0)) }
+  if (pain[1] === pain[4] && pain[1] === pain[7]) { dumb(dumber(1)) }
+  if (pain[2] === pain[5] && pain[2] === pain[8]) { dumb(dumber(2)) }
+  if (pain[0] === pain[4] && pain[0] === pain[8]) { dumb(dumber(0)) }
+  if (pain[6] === pain[4] && pain[6] === pain[2]) { dumb(dumber(6)) }
+  // else { alert('how are you this bad!? You tied! <br> CAT GAME!') }
+}
+
+function dumb () {
+  const element = document.getElementById('hiddenhweener')
+  element.style.display = 'block'
+}
+function dumber (i) {
+  const element = document.getElementById('board')
+  element.style.display = 'none'
+  const para = document.getElementById('identifyYourself')
+  para.textContent += pain[i]
+}
+// When the wheener function is activated, insert text(stampyBoi) in Divid identifyYourself
+// const para = document.getElementById('identifyYourself')
+// para.textContent += pain[7]
+// function leHide () {
+//   const element = document.getElementById('0')
+//   element.style.display = 'none'
+// }
+// create a gameboard, set values += curent player
+
+// if ($('#zero').value === 'o') {
+//   console.log('yar!')
+// }
+// function displayResult () {
+// if
+// (document.getElementById('zero').innerHTML = 'lol') { console.log('yolo') }
+// }
+// if ($('zero').attr() = 'x') {
+//   console.log('yolo')
+// }
+// if value is = on 0,1,2 we have a winner
+
+// if ($('zero') === 'x') {
+//   console.log('yolo')
+// }
+
+// })
+// if($('id').val() == Reset)
+
+// console.log($('#zero'))
+
+// if (document.getElementById('zero').innerHTML === 'x') {
+// console.log('uh-yup')
+// }
+// if (value )
+// const myEle = document.getElementById('zero')
+// if (myEle === 'null') {
+//   const myEleValue = myEle.value
+//   console.log(myEleValue + 'yey')
+// }
+// console.log(this.data('value')
